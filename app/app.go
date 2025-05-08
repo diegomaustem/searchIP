2df4e05be918a444b8388642123ece276ffe2837
diegomaustem <diego.maustem@gmail.com>
2025-05-07 22:00:16 -0300

    :sparkles: feat: Implementing search for ip

--- app/app.go
+++ app/app.go
@@ -1,10 +1,45 @@
 package app
 
-import "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
+
+	"github.com/urfave/cli"
+)
 
 func Generate() *cli.App {
 	app := cli.NewApp()
 	app.Name = "Application of comand line"
 	app.Usage = "Search IP'S and names of server in the internet"
+	
+	app.Commands = []cli.Command{
+		{
+			Name:  "ip",
+			Usage: "Search IP",
+			Flags: []cli.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Usage: "google.com",
+				},
+			},	
+			Action: searchIps,
+
+			},
+		}
+	
 	return app
+}
+
+func searchIps(c *cli.Context) {
+	host := c.String("host")
+	
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+	
+	for _, ip := range ips {
+		fmt.Println(ip)
+	}
 }
